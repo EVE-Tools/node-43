60fd3a9b8e9a5d70cc1d706c63eb8e3af999f9c3
Sebastian <info@zweizeichen.org>
2016-01-17 10:41:30 +0100

    Updated queries fpr Postgres 9.5

--- sql/patterns.sql
+++ sql/patterns.sql
@@ -40,3 +40,69 @@ WHERE orders.id = excluded.id AND orders.generated_at < excluded.generated_at
 INSERT INTO market_data_orderhistory (mapregion_id, invtype_id, numorders, low, high, mean, quantity, date)
 VALUES *values*
 ON CONFLICT (mapregion_id, invtype_id, date) DO NOTHING
+
+-- orderRegionStats
+
+INSERT INTO market_data_itemregionstat AS stats (buymean,
+  buyavg,
+  buymedian,
+  sellmean,
+  sellavg,
+  sellmedian,
+  buyvolume,
+  sellvolume,
+  buy_95_percentile,
+  sell_95_percentile,
+  mapregion_id,
+  invtype_id,
+  lastupdate,
+  buy_std_dev,
+  sell_std_dev)
+VALUES *values*
+ON CONFLICT (mapregion_id, invtype_id) DO UPDATE SET
+  buymean = excluded.buymean,
+  buyavg = excluded.buyavg,
+  buymedian = excluded.buymedian,
+  sellmean = excluded.sellmean,
+  sellavg = excluded.sellavg,
+  sellmedian = excluded.sellmedian,
+  buyvolume = excluded.buyvolume,
+  sellvolume = excluded.sellvolume,
+  buy_95_percentile = excluded.buy_95_percentile,
+  sell_95_percentile = excluded.sell_95_percentile,
+  lastupdate = excluded.lastupdate,
+  buy_std_dev = excluded.buy_std_dev,
+  sell_std_dev = excluded.sell_std_dev
+
+
+-- orderRegionStatHistory
+
+INSERT INTO market_data_itemregionstathistory AS stats (buymean,
+  buyavg,
+  buymedian,
+  sellmean,
+  sellavg,
+  sellmedian,
+  buyvolume,
+  sellvolume,
+  buy_95_percentile,
+  sell_95_percentile,
+  mapregion_id,
+  invtype_id,
+  date,
+  buy_std_dev,
+  sell_std_dev)
+VALUES *values*
+ON CONFLICT (mapregion_id, invtype_id, date) DO UPDATE SET
+  buymean = excluded.buymean,
+  buyavg = excluded.buyavg,
+  buymedian = excluded.buymedian,
+  sellmean = excluded.sellmean,
+  sellavg = excluded.sellavg,
+  sellmedian = excluded.sellmedian,
+  buyvolume = excluded.buyvolume,
+  sellvolume = excluded.sellvolume,
+  buy_95_percentile = excluded.buy_95_percentile,
+  sell_95_percentile = excluded.sell_95_percentile,
+  buy_std_dev = excluded.buy_std_dev,
+  sell_std_dev = excluded.sell_std_dev
